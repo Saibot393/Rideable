bc1338223e5345a691326f870da4adb60b9fb847
Saibot393 <137942782+Saibot393@users.noreply.github.com>
2023-07-01 14:06:06 +0200

    Added Familar Macro

--- scripts/MacroHooks.js
+++ scripts/MacroHooks.js
@@ -1,9 +1,10 @@
-import { MountSelected, UnMountSelected } from "./MountingScript.js";
+import { MountSelected, MountSelectedFamiliar, UnMountSelected } from "./MountingScript.js";
 
 //functions for macros
 Hooks.on("init",() => {
 	game.Rideable = {
 		MountSelected, //game.Rideable.MountSelected() -> Makes selected Tokens Ride targeted Token
+		MountSelectedFamiliar,
 		UnMountSelected
 	};
 });
