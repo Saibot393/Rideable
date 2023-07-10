9b2256016df83fdb5358a4ac71d814c5094f91b4
Saibot393 <137942782+Saibot393@users.noreply.github.com>
2023-07-10 19:22:44 +0200

    Translation + last changes

--- scripts/CoreVersionComp.js
+++ scripts/CoreVersionComp.js
@@ -4,11 +4,9 @@ export function sceneof(pToken) {
 	if (!vscene) {
 		//for FVTT v10
 		if (canvas.scene.tokens.get(pToken.id)) {
-			console.log("1", game.scenes.get(canvas.scene.id))
 			return canvas.scene;
 		}
 		else {
-			console.log("2", game.scenes.find(vscene => vscene.tokens.get(pToken.id)))
 			return game.scenes.find(vscene => vscene.tokens.get(pToken.id));
 		}
 	}
