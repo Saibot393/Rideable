50ad2ef29dcca96003fe0a8b695a188280151e7c
Saibot393 <137942782+Saibot393@users.noreply.github.com>
2023-11-03 14:38:47 +0100

    v2.15.0

--- scripts/compatibility/APIHandler.js
+++ scripts/compatibility/APIHandler.js
@@ -6,7 +6,7 @@ function withinBoundaries (pObject, pObjectForm, pPosition) {
 	//returns wether pPosition is within Boundaries of pObject with form pObjectForm("TokenFormCircle", "TokenFormRectangle", "TokenTransparency", "TokenFormAttachedTiles" (with token attacher))
 	return GeometricUtils.withinBoundaries(pObject, pObjectForm, pPosition);
 }
-
+	
 Hooks.once("init", () => {
 	game.modules.get(cModuleName).api = {
 		RideableFlags,
