a42d806857ebe488c648838a7dac040b54a750c9
Saibot393 <137942782+Saibot393@users.noreply.github.com>
2023-11-05 13:12:13 +0100

    v2.15.5

--- scripts/compatibility/APIHandler.js
+++ scripts/compatibility/APIHandler.js
@@ -6,12 +6,25 @@ function withinBoundaries (pObject, pObjectForm, pPosition) {
 	//returns wether pPosition is within Boundaries of pObject with form pObjectForm("TokenFormCircle", "TokenFormRectangle", "TokenTransparency", "TokenFormAttachedTiles" (with token attacher))
 	return GeometricUtils.withinBoundaries(pObject, pObjectForm, pPosition);
 }
+
+function RidingConnection(pObjecta, pObjectb, pSimple = false) {
+	//returns wether pObject1, pObject2 are connected via RideableConnected
+	if (pObjecta?.document && pObjectb?.document) {
+		console.log(pObjecta, pObjectb);
+		console.log(RideableFlags.RidingConnection(pObjecta.document, pObjectb.document, pSimple));
+		
+		return RideableFlags.RidingConnection(pObjecta.document, pObjectb.document, pSimple);
+	}
+	
+	return false //if anything fails
+}
 	
 Hooks.once("init", () => {
 	game.modules.get(cModuleName).api = {
 		RideableFlags,
 		Geometrics : {
 			withinBoundaries
-		}
+		},
+		RidingConnection
 	}
 });
