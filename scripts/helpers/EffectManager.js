c4dbb6431cd1ebc0fc70df6263fd8b802f8b03a7
Saibot393 <137942782+Saibot393@users.noreply.github.com>
2023-12-02 22:56:13 +0100

    Revert "Revert "v2.16.4""
    This reverts commit b4cf9f0d851383d478d28484c3f9ca6c52e3fa3f.

--- scripts/helpers/EffectManager.js
+++ scripts/helpers/EffectManager.js
@@ -35,9 +35,9 @@ class EffectManager {
 			//for riders effects
 			await EffectManager.removeRideableEffects(pRider, {grappleEffect : pRidingOptions.Grappled});
 			
-			if (pRidingOptions.MountingEffectOverride) {
-				if (pRidingOptions.MountingEffectOverride instanceof Array) {
-					vRiderEffectNames = pRidingOptions.MountingEffectOverride;
+			if (pRidingOptions.MountingEffectsOverride) {
+				if (pRidingOptions.MountingEffectsOverride instanceof Array) {
+					vRiderEffectNames = pRidingOptions.MountingEffectsOverride;
 				}
 			}
 			else {
